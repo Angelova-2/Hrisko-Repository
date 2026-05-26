5e5f747232261d4197956ff282718738e8879141
Hristiqn <xristiqnkostadinov@gmail.com>
2026-05-26 13:11:29 +0300

    Add files via upload

---
+++ zadachi/zadachi/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace zadachi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+
+        {
+
+
+            public class ArrayStack<T>
+        {
+            private T[] elements;
+
+            public int Count
+            {
+                get; private set;
+            }
+
+            private const int InitialCapacity = 16;
+
+            public ArrayStack(int capacity = InitialCapacity) 
+            {
+
+                elements = new T[capacity];
+
+            }
+            public void Push(T element) 
+            {
+                if (this.Count == this.elements.Length)
+                {
+                    this.Count++;
+                }
+
+               
+            }
+            public T Pop()  
+            {
+               if (this.Count == 0)
+
+            }
+            public T[] ToArray() 
+            { 
+                return new T[InitialCapacity];
+            }
+            private void Grow() 
+            {
+
+            }
+    }
+
+
+    }
+}
